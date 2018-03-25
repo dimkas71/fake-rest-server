b3c2bd2c1e55186310d6192f0301c491cf5777f0
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-03-25 18:12:24 +0300

    Adding WordsController class to generate a random list of names for using these in an Andorid app

---
+++ src/main/java/ua/compservice/controller/WordsController.java
@@ -0,0 +1,62 @@
+package ua.compservice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/words")
+public class WordsController {
+	
+	private static Logger logger = LoggerFactory.getLogger(WordsController.class.getName());
+	
+	private static final String[] NAMES = new String[10];
+	private static final int NUMBERS_BY_DEFAULT = 100;
+	
+	static {
+		NAMES[0] = "Dmitriy";
+		NAMES[1] = "Alex";
+		NAMES[2] = "Vasiliy";
+		NAMES[3] = "Vladik";
+		NAMES[4] = "Orest";
+		NAMES[5] = "Nikolas";
+		NAMES[6] = "John";
+		NAMES[7] = "Pamella";
+		NAMES[8] = "Irina";
+		NAMES[9] = "Bogdan";
+		
+	}
+	
+	
+	@GetMapping("{number}")
+	public List<String> words(@PathVariable("number") int number) {
+		
+		logger.info("Entered number is : {}", number);
+		
+		List<String> words = new ArrayList<>();
+		
+		Random randomizer = new Random();
+		
+		
+		int actualNumber = number <= 0 ? NUMBERS_BY_DEFAULT : number;
+		
+		logger.info("Actual used the number: {}", actualNumber);
+		
+		for (int i = 0; i < actualNumber; i++) {
+			words.add(NAMES[randomizer.nextInt(10)] + " " + String.valueOf(i));
+		}
+		
+		return words;
+		
+	}
+	
+
+}
