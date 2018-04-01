635b2c0311210f0b3514b163861821b051f785e2
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-04-01 15:11:56 +0300

    Adding methods latestValuesOf for ContractCreditRepository and CounterValuesOnPeriodRepository

--- src/main/java/ua/compservice/repository/ContractCreditRepository.java
+++ src/main/java/ua/compservice/repository/ContractCreditRepository.java
@@ -1,15 +1,29 @@
 package ua.compservice.repository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ua.compservice.model.Contract;
 import ua.compservice.model.ContractCredit;
 import ua.compservice.model.Counter;
 import ua.compservice.model.ServiceType;
 
-public interface ContractCreditRepository extends JpaRepository<ContractCredit, Long>{
-	ContractCredit findByDateAndContractAndServiceAndCounter(LocalDateTime d, Contract contract, ServiceType service, Counter counter);
+public interface ContractCreditRepository extends JpaRepository<ContractCredit, Long> {
+	ContractCredit findByDateAndContractAndServiceAndCounter(LocalDateTime d, Contract contract, ServiceType service,
+			Counter counter);
+
 	ContractCredit findByDateAndContractAndService(LocalDateTime d, Contract contract, ServiceType service);
+
+	@Query(value = "select id, credit, contract.date, contract.service, client_id, contract.contract_id, counter_id, trading_place_trading_place_id from contract_credit as contract\n"
+			+ "inner join\n" + "(SELECT max(date) as date, contract_id, service FROM CONTRACT_CREDIT \n"
+			+ "where contract_id in :contracts\n" + "group by contract_id, service) as inner_data\n" + "on \n"
+			+ "contract.date = inner_data.date\n" + "and contract.contract_id = inner_data.contract_id\n"
+			+ "and contract.service = inner_data.service", nativeQuery = true)
+	List<ContractCredit> latestValuesOf(@Param("contracts") Collection<Contract> contracts);
+
 }
--- src/main/java/ua/compservice/repository/CounterValuesOnPeriodRepository.java
+++ src/main/java/ua/compservice/repository/CounterValuesOnPeriodRepository.java
@@ -1,14 +1,25 @@
 package ua.compservice.repository;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ua.compservice.model.Counter;
 import ua.compservice.model.CounterValuesOnPeriod;
 
-public interface CounterValuesOnPeriodRepository extends JpaRepository<CounterValuesOnPeriod, Long>{
-	
+public interface CounterValuesOnPeriodRepository extends JpaRepository<CounterValuesOnPeriod, Long> {
+
 	CounterValuesOnPeriod findByPeriodAndCounter(LocalDate period, Counter c);
+
+	@Query(value = "select cvop.id, cvop.period, cvop.value, cvop.counter_id from COUNTER_VALUES_ON_PERIOD as cvop \n"
+			+ "inner join (SELECT max(period) as period, counter_id as counter_id FROM COUNTER_VALUES_ON_PERIOD \n"
+			+ "where counter_id in :counters\n" + "group by counter_id) inner_data\n"
+			+ "on cvop.period = inner_data.period\n"
+			+ "and cvop.counter_id = inner_data.counter_id", nativeQuery = true)
+	List<CounterValuesOnPeriod> latestValuesOf(@Param("counters") Collection<Counter> counters);
 
 }
